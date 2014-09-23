2445142c52964add4432daf103d1411e990a46b7
Rahil Dedhia <rahil.dedhia@students.olin.edu>
2014-09-23 11:07:09 -0400

    updated code to calibrate sensor

--- scanner/scanner.ino
+++ scanner/scanner.ino
@@ -1,28 +1,37 @@
 #include <Servo.h>
 
 //Variables
-int sensor;
+float sensor;
 int angle = 1;
 Servo myservo;
 
+// Csalibrating the sensor
+int i;
+int count;
+
 void setup()  { 
   Serial.begin(9600);
-  myservo.attach(9);
-  myservo.write(angle);
+//  myservo.attach(9);
+//  myservo.write(angle);
   pinMode(A0, INPUT);
 } 
 
 void loop()  {
-  for (angle; angle <= 90; angle++) {
-    Serial.println(angle);
-    myservo.write(angle);
-    delay(500);
+//  for (angle; angle <= 90; angle++) {
+//    Serial.println(angle);
+//    myservo.write(angle);
+//    delay(500);
+//  }
+//  angle = 1;
+  for (count = 0; count <=20; count++) {
+    for (i = 0; i < 5; i++) {
+      sensor += analogRead(A0);
+      delay(200);    
+    }
+    Serial.println(sensor / 5.0);
+    sensor = 0;
+    delay(5000);
   }
-  angle = 1;
-  
-  sensor = analogRead(A0);
-  //Serial.println(sensor);
-  delay(1000); 
 }
   
   

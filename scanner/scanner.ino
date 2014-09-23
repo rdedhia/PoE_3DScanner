cd378e06e3b8e95a8e8d508e42b666fe57daa924
Rahil Dedhia <rahil.dedhia@students.olin.edu>
2014-09-23 12:22:26 -0400

    got looping of servo angles working, as well as measurement collection

--- scanner/scanner.ino
+++ scanner/scanner.ino
@@ -1,37 +1,63 @@
 #include <Servo.h>
 
 //Variables
-float sensor;
-int angle = 1;
+int count = 0;
+int sensor;
+float sensor_avg;
+byte angle = 20;
+byte angle2 = 20;
 Servo myservo;
-
-// Csalibrating the sensor
-int i;
-int count;
+Servo myservo2;
+const byte servos[] = {9,10};
+byte interval = 80;
+long previousMillis = 0;
+long currentMillis;
 
 void setup()  { 
   Serial.begin(9600);
-//  myservo.attach(9);
-//  myservo.write(angle);
+  myservo.attach(servos[0]);
+  myservo2.attach(servos[1]);
   pinMode(A0, INPUT);
 } 
 
-void loop()  {
-//  for (angle; angle <= 90; angle++) {
-//    Serial.println(angle);
-//    myservo.write(angle);
-//    delay(500);
-//  }
-//  angle = 1;
-  for (count = 0; count <=20; count++) {
-    for (i = 0; i < 5; i++) {
-      sensor += analogRead(A0);
-      delay(200);    
+void loop()  {  
+  for (angle; angle < 60; angle++) {
+    myservo.write(angle);
+    for (angle2; angle2 < 60; angle2++) {
+      print_angles();
+      sensor_avg = measure();
     }
-    Serial.println(sensor / 5.0);
-    sensor = 0;
-    delay(5000);
+    angle++;
+    myservo.write(angle);
+    for (angle2; angle2 > 20; angle2--) {
+      print_angles();
+      sensor_avg = measure();
+    }
   }
 }
+
+void print_angles() {
+  Serial.print("Angle 1: ");
+  Serial.print(angle);
+  Serial.print(" Angle 2: ");
+  Serial.println(angle2);
+  myservo2.write(angle2);
+}
   
+float measure() {
+  count = 0;
+  sensor = 0;
   
+  while (count < 5) {
+    currentMillis = millis();
+    if (currentMillis - previousMillis > interval) {
+      sensor += analogRead(A0);
+      previousMillis = currentMillis;
+      count += 1;
+    }    
+  }
+  float sensor_avg = sensor / 5.0;
+  Serial.print("Sensor average: ");
+  Serial.println(sensor_avg);
+  return sensor_avg;
+}
